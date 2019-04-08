b2f43135082d1be4034f5e1d04256f98f20c13c8
Lauren Irish <lirish2@gmail.com>
2019-04-08 15:52:38 -0600

    solutions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showMenu: false
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div>
+        <header className="main-header">
+        <div className={this.state.showMenu ? 'menu slide' : 'menu'}>
+          <span>SERVICES</span>
+          <span>PORTFOLIO</span>
+          <span>ABOUT</span>
+          <span>TEAM</span>
+          <span>CONTACT</span>
+        </div>
+          <nav className="nav-bar">
+            <div className="title">Start Bootstrap</div>
+            <div className="links-container">
+              <span>SERVICES</span>
+              <span>PORTFOLIO</span>
+              <span>ABOUT</span>
+              <span>TEAM</span>
+              <span>CONTACT</span>
+            </div>
+            <div 
+              className="ham-menu"
+              onClick={() => this.setState({showMenu: !this.state.showMenu})}
+              >MENU &#9776;</div>
+          </nav>
+          <div className="center-text">
+            <h3>Welcome To Our Studio!</h3>
+            <h1>IT'S NICE TO MEET YOU</h1>
+            <button>TELL ME MORE</button>
+          </div>
+         
         </header>
       </div>
     );
